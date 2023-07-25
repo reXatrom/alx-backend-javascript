8b693550576556b0ab2562d7a0e15139356bcf98
rexatrom <ajstylejoshua2837@gmail.com>
2023-07-25 00:06:29 +0000

    fixed some bugs

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,6 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-	/* arrow was missing in the original code */
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
